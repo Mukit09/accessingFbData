a93a16cf738a3694409f4d52c0bc9b8e8a48c868
Mukit09 <mukitmkbs25@yahoo.com>
2018-02-24 23:20:47 +0600

    accessing facebook data

---
+++ src/main/java/com/development/accessingFbData/AccessingFbDataApplication.java
@@ -0,0 +1,12 @@
+package com.development.accessingFbData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingFbDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingFbDataApplication.class, args);
+	}
+}
